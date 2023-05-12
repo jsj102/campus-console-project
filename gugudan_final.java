0da78d1c8ae77bbbafc25542017acfa426853d99
hmmwtf <94499659+hmmwtf@users.noreply.github.com>
2023-05-12 10:29:13 +0900

    Update gugudan_final.java
    try catch문을 이용해 보았습니다.

--- gugudan_final.java
+++ gugudan_final.java
@@ -6,20 +6,32 @@ public class gugudan_final {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
-		int x = scanner.nextInt();
-		
-		for(int i = 1; i <= 9; i++){
+
+		int x = 0;
+
+		while (true) {
+			try {
+				System.out.println("---구구단---");
+				System.out.println("몇 단을 원하세요?");
+
+				x = scanner.nextInt();
+
+				if (x < 1 || x > 999) {
+					// 잘못된 입력값 처리
+					throw new IllegalArgumentException("1~999 범위의 정수를 입력해 주세요.");
+				}
+				break;
+			} catch (IllegalArgumentException e) {
+				System.out.println("1~999 범위가 아닙니다.");
+				System.out.println(e.getMessage());
+				continue;
+			}
+		}
+
+		// 1~9을 곱하여 출력합니다.
+		for (int i = 1; i <= 9; i++) {
 			System.out.println(x + " x " + i + " = " + (x * i));
 		}
-		
-		while (true) {
-			if(x < 1 || x >999) {
-				System.out.println("error -> input data is wrong");
-				break;
-			}
-		
-		}
-	}
 
+	}
 }
